5608f89a968b919d300bf1f557f5a75ec39628cd
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2025-03-07 11:41:06 +0545

    prefetching in pagination

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,28 +1,38 @@
 import { getToday } from "../utils/helpers";
 import supabase from "../utils/supabase";
+import { PAGE_SIZE } from "../utils/constants";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" }
     );
 
-  ///filter
-  if (filter) query = query.eq(filter.field, filter.value);
+  // FILTER
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
-  //sort
+  // SORT
   if (sortBy)
-    query = query[filter.method || "eq"](sortBy.field, {
+    query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
-  if (error) {
-    console.log(error);
-    throw new Error("Bookings could not be created");
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
   }
-  return data;
+
+  const { data, error, count } = await query;
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return { data, count };
 }
 
 export async function getBooking(id) {
@@ -41,6 +51,7 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+// date: ISOString
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
@@ -60,7 +71,6 @@ export async function getBookingsAfterDate(date) {
 export async function getStaysAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    // .select('*')
     .select("*, guests(fullName)")
     .gte("startDate", date)
     .lte("startDate", getToday());
