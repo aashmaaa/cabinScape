220575e74beb153bd18551683d387b94bdfd217f
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2025-03-10 23:10:37 +0545

    changes

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://thhnhpgvnoiiydbkgqli.supabase.co";
-const supabaseKey =
+export const supabaseUrl = "https://thhnhpgvnoiiydbkgqli.supabase.co";
+export const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoaG5ocGd2bm9paXlkYmtncWxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NDQwMDAsImV4cCI6MjA1NTAyMDAwMH0.r2CEA1D2AqtQo061jEgGvAhRn3U4kkmRknoTOID996Y";
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
 //  = createClient(supabaseUrl, supabaseKey);
